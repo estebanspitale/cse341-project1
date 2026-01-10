60ea0ae8452c4044e526db704cb6bcc34a7cbb52
Esteban Spitale <estebanspitale089@gmail.com>
2026-01-10 08:54:02 -0300

    connectiondb

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,9 +1,33 @@
-const getAllContacts = (req, res) => {
-  res.json({ message: 'Get all contacts works' });
+const mongodb = require('../db/connect');
+const ObjectId = require('mongodb').ObjectId;
+
+const getAllContacts = async (req, res) => {
+  try {
+    const db = mongodb.getDb().db('cse341');
+    const contacts = await db
+      .collection('contacts')
+      .find()
+      .toArray();
+
+    res.status(200).json(contacts);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
-const getSingleContact = (req, res) => {
-  res.json({ message: `Get contact ${req.params.id} works` });
+const getSingleContact = async (req, res) => {
+  try {
+    const db = mongodb.getDb().db('cse341');
+    const contactId = new ObjectId(req.params.id);
+
+    const contact = await db
+      .collection('contacts')
+      .findOne({ _id: contactId });
+
+    res.status(200).json(contact);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
 module.exports = {
--- server.js
+++ server.js
@@ -1,21 +1,29 @@
+console.log('SERVER FILE LOADED');
+
 const express = require('express');
 const app = express();
 require('dotenv').config();
 
+const mongodb = require('./db/connect');
 const contactsRoutes = require('./routes/contacts');
 
 app.use(express.json());
 
-// Ruta requerida por el PASO 2
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-// Rutas del proyecto
 app.use('/contacts', contactsRoutes);
 
 const port = process.env.PORT || 8080;
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('ABOUT TO START SERVER');
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  }
 });
