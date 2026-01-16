a4b67f68216652cc60a7cc76c753499de346931f
Esteban Spitale <estebanspitale089@gmail.com>
2026-01-16 16:51:51 -0300

    Add POST PUT DELETE contacts routes

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,6 +1,9 @@
 const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
+/* ======================
+   GET ALL CONTACTS
+====================== */
 const getAllContacts = async (req, res) => {
   try {
     const db = mongodb.getDb().db('cse341');
@@ -15,6 +18,9 @@ const getAllContacts = async (req, res) => {
   }
 };
 
+/* ======================
+   GET SINGLE CONTACT
+====================== */
 const getSingleContact = async (req, res) => {
   try {
     const db = mongodb.getDb().db('cse341');
@@ -30,7 +36,98 @@ const getSingleContact = async (req, res) => {
   }
 };
 
+/* ======================
+   POST - CREATE CONTACT
+====================== */
+const createContact = async (req, res) => {
+  try {
+    const db = mongodb.getDb().db('cse341');
+
+    const contact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday
+    };
+
+    // Validación: todos requeridos
+    if (
+      !contact.firstName ||
+      !contact.lastName ||
+      !contact.email ||
+      !contact.favoriteColor ||
+      !contact.birthday
+    ) {
+      return res.status(400).json({ message: 'All fields are required.' });
+    }
+
+    const response = await db
+      .collection('contacts')
+      .insertOne(contact);
+
+    res.status(201).json({ id: response.insertedId });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+/* ======================
+   PUT - UPDATE CONTACT
+====================== */
+const updateContact = async (req, res) => {
+  try {
+    const db = mongodb.getDb().db('cse341');
+    const contactId = new ObjectId(req.params.id);
+
+    const contact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday
+    };
+
+    const response = await db
+      .collection('contacts')
+      .replaceOne({ _id: contactId }, contact);
+
+    if (response.modifiedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(400).json({ message: 'Contact not updated.' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+/* ======================
+   DELETE CONTACT
+====================== */
+const deleteContact = async (req, res) => {
+  try {
+    const db = mongodb.getDb().db('cse341');
+    const contactId = new ObjectId(req.params.id);
+
+    const response = await db
+      .collection('contacts')
+      .deleteOne({ _id: contactId });
+
+    if (response.deletedCount > 0) {
+      res.status(204).send();
+    } else {
+      res.status(400).json({ message: 'Contact not deleted.' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
 module.exports = {
   getAllContacts,
-  getSingleContact
+  getSingleContact,
+  createContact,
+  updateContact,
+  deleteContact
 };
--- routes/contacts.js
+++ routes/contacts.js
@@ -2,7 +2,17 @@ const express = require('express');
 const router = express.Router();
 const contactsController = require('../controllers/contacts');
 
+// GET
 router.get('/', contactsController.getAllContacts);
 router.get('/:id', contactsController.getSingleContact);
+
+// POST
+router.post('/', contactsController.createContact);
+
+// PUT
+router.put('/:id', contactsController.updateContact);
+
+// DELETE
+router.delete('/:id', contactsController.deleteContact);
 
 module.exports = router;
